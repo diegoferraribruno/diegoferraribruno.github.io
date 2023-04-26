6dc6f8f19fa5a55e26fde2dd49975d343ffd9451
Diego Ferrari Bruno <diegodesenha@gmail.com>
2023-04-26 00:05:48 -0300

    1.9.8 willreadfrequently, move frames arrows

--- js/camera.js
+++ js/camera.js
@@ -247,8 +247,9 @@ function trocaCamera() {
 
 }
 
+
 function isCanvasBlank(canvas) {
-    const context = canvas.getContext('2d');
+
     const pixelBuffer = new Uint32Array(context.getImageData(0, 0, canvas.width, canvas.height).data.buffer);
     return !pixelBuffer.some(color => color !== 0);
 }
