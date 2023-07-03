4f08e4fe550c73d1d4762eab36c2595114a0896c
Diego Ferrari Bruno <diegodesenha@gmail.com>
2023-07-03 13:43:44 -0300

    4.5 confirm leave page

--- js/start.js
+++ js/start.js
@@ -184,3 +184,16 @@ function apresenta() {
   }
 
 }
+function goodbye(e) {
+  if (!e) e = window.event;
+  //e.cancelBubble is supported by IE - this will kill the bubbling process.
+  e.cancelBubble = true;
+  e.returnValue = 'You sure you want to leave?'; //This is displayed on the dialog
+
+  //e.stopPropagation works in Firefox.
+  if (e.stopPropagation) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+}
+window.onbeforeunload = goodbye; 
