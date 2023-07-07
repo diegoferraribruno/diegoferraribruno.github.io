6539885bd4f4e32827f31ff506dcc2c8e21b5d50
Diego Ferrari Bruno <diegodesenha@gmail.com>
2023-07-07 11:17:19 -0300

    5.4 redimensionar for 1 frame

--- js/redimensionar.js
+++ js/redimensionar.js
@@ -1,4 +1,32 @@
+const canvasWidthInput = document.getElementById('canvasWidth');
+const canvasHeightInput = document.getElementById('canvasHeight');
+canvasWidthInput.addEventListener('input', updateHeightField);
+const resizeButton = document.getElementById('resizeButton');
+resizeButton.addEventListener('click', resizeCanvas);
+
 function preResizeCanvas(newWidth, newHeight) {
+    if (!newHeight || !newWidth) {
+        newWidth = parseInt(document.getElementById('canvasWidth').value);
+        newHeight = parseInt(document.getElementById('canvasHeight').value);
+    }
+    // Clear the canvas
+    canvasFront.ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Set the canvas size to the new values
+    // canvasFront.width = newWidth;
+    //canvasFront.height = newHeight;
+    desenhaRetangulo(0, 0, newWidth, newHeight)
+
+    // Draw the image on the canvas (image will be resized to fit the new canvas size)
+    canvasFront.ctx.drawImage(canvas, 0, 0, newWidth, newHeight);
+    // Reset the selection paths and current path
+    canvasFront.classList.remove("esconde")
+}
+
+
+
+
+function resizeCanvas(newWidth, newHeight) {
     if (!newHeight || !newWidth) {
         newWidth = parseInt(document.getElementById('canvasWidth').value);
         newHeight = parseInt(document.getElementById('canvasHeight').value);
@@ -9,28 +37,26 @@ function preResizeCanvas(newWidth, newHeight) {
     // Set the canvas size to the new values
     canvasFront.width = newWidth;
     canvasFront.height = newHeight;
+    canvasFront.style.marginLeft = -newWidth + "px"
+    canvasDiv.style.width = newWidth + "px"; //add 30px for scroll
+    canvasDiv.style.height = newHeight + "px"; //add 30px for scroll
+    canvasBack.width = newWidth;
+    canvasBack.height = newHeight;
+    canvasBack.style.marginLeft = -newWidth + "px"
+    iD("bplayer0").style.width = newWidth + "px"
+    iD("bplayer0").style.height = newHeight + "px"
+    iD("bplayer0").style.marginTop = - newHeight - 4 + "px"
 
-    // Draw the image on the canvas (image will be resized to fit the new canvas size)
-    canvasFront.ctx.drawImage(image, 0, 0, newWidth, newHeight);
-    // Reset the selection paths and current path
-}
+    canvasFront.ctx.drawImage(canvas, 0, 0, newWidth, newHeight);
+
+    setTimeout(() => {
 
 
-
-function resizeCanvas(newWidth, newHeight) {
-    if (!newHeight || !newWidth) {
-        newWidth = parseInt(document.getElementById('canvasWidth').value);
-        newHeight = parseInt(document.getElementById('canvasHeight').value);
-    }
-    // Clear the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Set the canvas size to the new values
-    canvas.width = newWidth;
-    canvas.height = newHeight;
-
-    // Draw the image on the canvas (image will be resized to fit the new canvas size)
-    ctx.drawImage(image, 0, 0, newWidth, newHeight);
+        // Draw the image on the canvas (image will be resized to fit the new canvas size)
+        canvas.width = newWidth;
+        canvas.height = newHeight;
+        context.drawImage(canvasFront, 0, 0, newWidth, newHeight);
+    }, canvas.height / 2)
 
     // Reset the selection paths and current path
     // selectionPaths = [];
@@ -45,4 +71,5 @@ function updateHeightField() {
     const newHeight = Math.round(width / currentAspectRatio);
 
     canvasHeightInput.value = newHeight;
+    preResizeCanvas(width, newHeight)
 }
